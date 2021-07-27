bd16ee273e48aca6a4b355f79f9e2eee91d36fd4
Armand <girier.armand@gmail.com>
2021-07-27 20:06:06 +0900

    Adding tailwind for styles.

--- pages/aptitudes.js
+++ pages/aptitudes.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import 'tailwindcss/tailwind.css'
 
 
 export async function getServerSideProps({ query, req, res }) {
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import 'tailwindcss/tailwind.css'
 
 export async function getServerSideProps({ params, req, res }) {
   
